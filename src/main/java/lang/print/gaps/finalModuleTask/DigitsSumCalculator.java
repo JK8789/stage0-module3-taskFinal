0d85edfa6ff481dcf484f99e4386ab337d89df8c
Korotkova Julia <korotkovayuliav@gmail.com>
2022-09-14 10:30:50 +0200

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,19 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int firstDigitOfNumber = number / 1000;
+        int secondDigitOfNumber = (number - firstDigitOfNumber * 1000) / 100;
+        int thirdDigitOfNumber = (number - firstDigitOfNumber * 1000
+                                             - secondDigitOfNumber * 100) / 10;
+        int forthDigitOfNumber = number - firstDigitOfNumber * 1000
+                                        - secondDigitOfNumber * 100
+                                        - thirdDigitOfNumber * 10;
+        System.out.println(firstDigitOfNumber + secondDigitOfNumber
+                         + thirdDigitOfNumber + forthDigitOfNumber);
+    }
+
+    public static void main(String[] args) {
+        DigitsSumCalculator digitsSumCalculator = new DigitsSumCalculator();
+        digitsSumCalculator.calculateSum(1234);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public static void revert(int number) {
+        int firstDigit = number / 100;
+        int secondDigit = (number-(firstDigit*100))/10;
+        int thirdDigit = number - firstDigit * 100 - secondDigit * 10;
+        int result = thirdDigit*100 + secondDigit*10 + firstDigit;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        revert(123);
     }
 }
